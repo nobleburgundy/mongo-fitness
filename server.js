0f1ac9703aa211a0d2679a0c17b8440f22f74d6c
James Gould <nobleburgundy@gmail.com>
2021-02-06 22:36:30 -0600

    Updated connection for heroku deploys.

--- server.js
+++ server.js
@@ -20,7 +20,12 @@ app.use(express.static("public"));
 app.use(api_controller);
 app.use(html_controller);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
