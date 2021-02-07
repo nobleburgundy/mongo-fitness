baf07c6d24e954cdb2c56c92fa965d3c5a1739c5
James Gould <nobleburgundy@gmail.com>
2021-02-06 22:26:42 -0600

    Added default date of date.now to Workout model.

--- models/Workout.js
+++ models/Workout.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  day: Date,
+  day: { type: Date, default: Date.now },
   exercises: Array,
 });
 
