ac822dc6b30d290141327994c50df0b9584fd112
James Gould <nobleburgundy@gmail.com>
2021-03-13 07:40:20 -0600

    Updated Cypress tests

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -3,12 +3,14 @@ before(() => {
 });
 
 describe("Home page", () => {
-  it("Mongo Fitness Header Loads", () => {
+  it("Mongo Fitness Home Page Loads", () => {
     cy.get("h1").should("be.visible").and("contain", "Fitness Tracker");
-  });
-
-  it("New and Continue Workout buttons load", () => {
     cy.contains("Continue Workout").should("be.visible");
     cy.contains("New Workout").should("be.visible");
+    cy.get("a").contains("Dashboard").should("be.visible");
+    cy.get("a").contains("Fitness Tracker").should("be.visible");
   });
+
+  //   it("New and Continue Workout buttons load", () => {
+  //   });
 });
