18c6f9ef19fc739c72ec7ac3d19a532a4c41627b
James Gould <nobleburgundy@gmail.com>
2021-03-13 07:55:51 -0600

    Testing travis and cypress

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -5,10 +5,10 @@ before(() => {
 describe("Home page", () => {
   it("Mongo Fitness Home Page Loads", () => {
     cy.get("h1").should("be.visible").and("contain", "Fitness Tracker");
-    cy.contains("Continue Workout").should("be.visible");
-    cy.contains("New Workout").should("be.visible");
-    // cy.get("a").contains("Dashboard").should("be.visible");
-    // cy.get("a").contains("Fitness Tracker").should("be.visible");
+    // cy.contains("Continue Workout").should("be.visible");
+    // cy.contains("New Workout").should("be.visible");
+    cy.get("a").contains("Dashboard").should("be.visible");
+    cy.get("a").contains("Fitness Tracker").should("be.visible");
   });
 
   //   it("New and Continue Workout buttons load", () => {
